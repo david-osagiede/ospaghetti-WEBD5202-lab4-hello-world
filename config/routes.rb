382782d7f2348ad8aee5873df83b07655d02f1ec
david osagiede <yungospaghetti@gmail.com>
2022-11-22 11:17:47 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # You can have the root of your site routed with "root"
+  root 'application#hello'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
